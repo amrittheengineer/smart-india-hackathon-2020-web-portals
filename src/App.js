3a9d39567eddb01a9b14716c8143bc98c5fda402
Vignesh Karthikeyan <imvigneshkarthikeyan@gmail.com>
2020-07-18 21:06:26 +0530

    Added all the 5 sidebars

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import MainSidebar from "./Components/MainSidebar"; 
+import VisitorSidebar from "./Components/VisitorSidebar";
+import InstituteSidebar from "./Components/InstituteSidebar";
+import StudentSidebar from "./Components/StudentSidebar";
+import StaffSidebar from "./Components/StaffSidebar";
 
 function App() {
   return (
