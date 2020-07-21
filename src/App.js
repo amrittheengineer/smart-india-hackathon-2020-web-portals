db6d03633306965988a9dea41c30eaa54e1b27ea
Vignesh Karthikeyan <imvigneshkarthikeyan@gmail.com>
2020-07-21 13:34:35 +0530

    Added VisitCard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import EducationalOfficerPage from "./Pages/EducationalOfficerPage";
 import SchoolPage from "./Pages/SchoolPage";
-import VisitCard from "./Components/VisitCard";
 
 function App() {
   return (
     <div>
-      <VisitCard />
+      <EducationalOfficerPage />
     </div>
   );
 }
